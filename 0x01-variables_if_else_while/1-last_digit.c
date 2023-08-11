593704bf1133674ebf567d5d9cdea129a7d1ce22
P4ver <s.pales20@gmail.com>
2023-08-11 16:46:03 -0700

    hard

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ *main - the entry point 
+ *
+ *Description: project to get the last dgit of the nmbr
+ *Return: 0 to work successfully
+ */
 int main(void)
 {
 	int n;
@@ -15,9 +18,8 @@ int main(void)
 	if (n > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_d);
 	else if (n < 6)
-		printf("Last digit of %d is %d and is less than 6 and not 0", n, last_d);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_d);
 	else
-		printf("Last digit of %d is 0 and is 0", n);
-
+		printf("Last digit of %d is 0 and is 0\n", n);
 	return (0);
 }
