6807bab86a46a62886a87fc944f843d2bbe159b2
P4ver <s.pales20@gmail.com>
2023-08-11 18:49:18 -0700

    carry on

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,15 +2,16 @@
 /**
  *main - the entry point
  *
- *print numbers 0 to 9
+ *Description: print num using putchar
  *Return: 0 to work successfully
  */
 int main(void)
 {
 	int i;
+	char nu[20] = "0123456789";
 
 	for (i = 0; i <= 9; i++)
-		printf("%d", i);
-	printf("\n");
+		putchar(nu[i]);
+	putchar('\n');
 	return (0);
 }
