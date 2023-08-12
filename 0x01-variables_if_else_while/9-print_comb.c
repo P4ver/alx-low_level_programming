a358354e79f21725dfb10ce93e035737ee745fce
P4ver <s.pales20@gmail.com>
2023-08-11 19:53:35 -0700

    adv

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+/**
+ *main - the entry point
+ *
+ *Discription: making combination for success
+ *
+ *Return: 0 to work success
+ */
 int main(void)
 {
 	int i;
-	for(;;)
-	putchar('');	
+
+	for (i = 0 ; i <= 9; i++)
+	{
+		putchar(i + 48);
+		putchar(44);
+		putchar(32);
+	}
+	putchar('\n');
+	return (0);
 }
