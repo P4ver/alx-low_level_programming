a0c974af005395ba13b84111e38e0c7a4f922ce1
P4ver <s.pales20@gmail.com>
2023-08-11 18:26:24 -0700

    doing hard things

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ *main - the entry point
+ *
+ *print numbers 0 to 9
+ *Return: 0 to work successfully
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
