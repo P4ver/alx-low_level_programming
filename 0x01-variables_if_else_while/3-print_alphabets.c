fedee644a513777e2c7dbff15888f7c7c3f412cd
P4ver <s.pales20@gmail.com>
2023-08-11 17:58:36 -0700

    gg

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,10 +10,11 @@ int main(void)
 {
 	int i;
 	char a_u_l[60] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	
+
 	for (i = 0; i <= 51; i++)
 	{
 		putchar(a_u_l[i]);
 	}
+	putchar('\n');
 	return (0);
 }
