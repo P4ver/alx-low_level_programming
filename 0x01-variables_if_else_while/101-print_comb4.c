469438cb2463580dff33bff32bdb41cffb47e9fd
P4ver <s.pales20@gmail.com>
2023-08-12 16:40:42 -0700

    the whole day for C

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ *main - the entry point
+ *
+ *Description: we use int c to increment every row
+ *	inside three nested loops
+ *
+ *Return: always 0 to success
+ */
+int main(void)
+{
+	int c = 1;
+
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9 - i; j++)
+		{
+			for (int k = 0; k < 9 - j; k++)
+			{
+				if (j + c == k + j + c)
+					continue;
+				if (k + j + c > 9)
+					break;
+				putchar (i + 48);
+				putchar (j + c + 48);
+				putchar (k + j + c + 48);
+				if (i == 7)
+					break;
+				putchar (44);
+				putchar (32);
+			}
+		}
+		c = c + 1;
+	}
+	putchar ('\n');
+	return (0);
+}
