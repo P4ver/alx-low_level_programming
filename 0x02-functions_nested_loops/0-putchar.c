3609c584727dcc7097ef108afcedd76c4ce155b2
P4ver <s.pales20@gmail.com>
2023-08-15 11:55:00 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *main - the entry point
+ *
+ *-putchar -  print the c character  to stdout'
+ *
+ *Return: 0 to work successufully
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
