5eaf44629c5d6014801cdf2db332b1dd1ab35a10
P4ver <s.pales20@gmail.com>
2023-08-15 13:02:19 -0700

    1 task c lw lvl

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *main - the entry point ,
+ *
+ *Description : print the alphabet a-z.
+ *
+ *Return: 0 always
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
