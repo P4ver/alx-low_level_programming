de87839d1cbda9e5bba806846eebf0d4860abed5
P4ver <s.pales20@gmail.com>
2023-08-16 11:42:02 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,23 +1,19 @@
 #include "main.h"
 /**
+ * times_table - function prints 9 times table
  *
- *
- *
- *
- *
- *
- *
+ *Description: we use loop nested and condition if
  */
 void times_table(void)
 {
 	int x, y, u;
 
-	for(x = 0; x <= 9; x++)
+	for (x = 0; x <= 9; x++)
 	{
-		for(y = 0; y <= 9; y++)
+		for (y = 0; y <= 9; y++)
 		{
 			u = x * y;
-			if(u >= 10)
+			if (u >= 10)
 			{
 				_putchar(44);
 				_putchar(32);
@@ -26,7 +22,7 @@ void times_table(void)
 			}
 			else
 			{
-				if((u < 10) && y != 0)
+				if ((u < 10) && y != 0)
 				{
 					_putchar(44);
 					_putchar(32);
