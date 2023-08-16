caef4bcdf86d4b2889c9184efe67f9e86a0a9136
P4ver <s.pales20@gmail.com>
2023-08-16 11:56:07 -0700

    the sum

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
+ * add - function prints the sum of two integers,
  *
+ *@n: the value of the number that will be calculated
+ *@p: the number as variable n
  *
- *
- *
- *
- *
- *
+ * Return: the valueof the summation
  */
 int add(int n, int p)
 {
