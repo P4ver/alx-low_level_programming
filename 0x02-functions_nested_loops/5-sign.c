fbce152f8895b06adbdff4fb177b127c88fa9d8d
P4ver <s.pales20@gmail.com>
2023-08-15 17:18:37 -0700

    task 5 lw lvl

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - function prints the sign of numb
+ *
+ *@n: the number which we want to know its sign
+ *
+ *Return: 1 if the numn is positive
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+}
