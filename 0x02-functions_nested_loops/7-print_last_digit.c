9f589d3aa15a995f8c725bc117859102d747b076
P4ver <s.pales20@gmail.com>
2023-08-16 06:26:18 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit - function that prints last Digit
+ *
+ *@w: the number which the last digit will be
+ *	determined
+ *@last_d: will computes the modulo
+ *
+ *Return: will return the value of last digit
+ */
+int print_last_digit(int w)
+{
+	int last_d = w % 10;
+
+	if (w < 0)
+		last_d = last_d * (-1);
+	_putchar('0' + last_d);
+	return (last_d);
+}
