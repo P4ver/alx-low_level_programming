9e2446e8954a71042dc662d28b6ebeffbcf94b16
P4ver <s.pales20@gmail.com>
2023-08-15 18:14:05 -0700

    more c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - it's a function computes the absulte
+ *	value of int
+ *@int: the val that well be computed the abs
+ *
+ *Return: p*(-1) will be positive
+ */
+int _abs(int)
+{
+	if (p < 0)
+		return (p * (-1));
+	else
+		return (p);
+}
