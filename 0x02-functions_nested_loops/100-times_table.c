a07eb96776ba143a83f5c8ee2bf95d18dd542892
P4ver <s.pales20@gmail.com>
2023-08-16 18:34:32 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,41 +6,33 @@
  */
 void print_times_table(int n)
 {
-	if (n >= 0 && n < 15){
+	if (n >= 0 && n < 15)
+	{
 		int m, k, p;
 
 		for (m = 0; m <= n; m++)
 		{
-			for (k = 0; k <= n; k++)
+			_putchar('0');
+			for (k = 1; k <= n; k++)
 			{
+				_putchar(',');
+				_putchar(' ');
 				p = m * k;
+				if (p <= 99)
+					_putchar(' ');
+				if (p <= 9)
+					_putchar(' ');
+
 				if (p > 9 && p <= 99)
 				{
-					_putchar(44);
-					_putchar(32);
-					_putchar(32);
 					_putchar('0' + (p / 10));
-					_putchar('0' + (p % 10));
 				}
 				else if (p > 99)
 				{
-					_putchar(44);
-					_putchar(32);
 					_putchar('0' + (p / 100));
 					_putchar('0' + (p / 10) % 10);
-					_putchar('0' + (p % 10));
 				}
-				else
-				{
-					if (k != 0 && p < 99)
-					{
-						_putchar(44);
-						_putchar(32);
-						_putchar(32);
-						_putchar(32);
-					}
-					_putchar('0' + p);
-				}
+				_putchar('0' + (p % 10));
 			}
 		_putchar('\n');
 		}
