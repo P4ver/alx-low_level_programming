ed56ef94bc9215df0918181d8f331f680268bf8a
P4ver <s.pales20@gmail.com>
2023-09-21 15:12:05 -0700

    tsk

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,12 +8,9 @@ size_t print_list(const list_t *h)
 {
 	size_t cnt_xx = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		if ((h->str) == NULL)
-			printf("[0] (nill)\n");
-		else
-			printf("[%u] %s\n", h->len, h->str);
+		printf("[%u] %s\n", (h->str ? h->len : 0), (h->str ? h->str : "(nil)"));
 		h = h->next;
 		cnt_xx++;
 	}
