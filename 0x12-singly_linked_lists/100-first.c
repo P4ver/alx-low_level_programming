1c869288243d90660da75b3c88ab017d8f7148cf
P4ver <s.pales20@gmail.com>
2023-09-21 17:25:34 -0700

    pr

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,6 +5,6 @@ void m_construct_funct(void) __attribute__((constructor));
  */
 void m_construct_funct(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-		I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
