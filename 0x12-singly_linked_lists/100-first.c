01c51137e1096585f43e91b8cfb12d08acbecc55
P4ver <s.pales20@gmail.com>
2023-09-21 17:16:06 -0700

    tsk

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void m_construct_funct(void) __attribute__((constructor));
+/**
+ * m_construct_funct - construct to print a msg,
+ */
+void m_construct_funct(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+		I bore my house upon my back!\n");
+}
