edd895cc23d4fae2971f8b40055b70159c3e05fb
P4ver <s.pales20@gmail.com>
2023-09-28 08:35:09 -0700

    ftdk

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,22 +1,22 @@
 #include "main.h"
 /**
- * binary_to_uint - Write a function that converts a binary number to an unsigned int,
+ * binary_to_uint - Write a function that converts a binary
+ *	number to an unsigned int,
  * @b: ptn=r to string,
  * Return: the string b that is not 0 or 1 b is NULL,
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int q, nm_x;
+	unsigned int res_ = 0;
 
 	if (b == NULL)
 		return (0);
-	for (q = 0; b[q]; q++)
+	while (*b)
 	{
-		if (b[q] != '0' && b[q] != '1')
+		if (*b != '0' && *b != '1')
 			return (0);
+		res_ = (res_ << 1) + (*b - '0');
+		b++;
 	}
-	nm_x = 0;
-	for (q = 0; b[q]; q++)
-		nm_x = nm_x * 2 + (b[q] - '0');
-	return (nm_x);
+	return (res_);
 }
