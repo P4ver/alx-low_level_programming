74f3b12001e078fb42db6f1f6ae10aa37abf29eb
P4ver <s.pales20@gmail.com>
2023-10-03 11:03:42 -0700

    hg

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,7 +7,8 @@
  */
 int main(int cc, char **cv)
 {
-	int f_frm, f_to, n_read;
+	int f_frm = 0, f_to = 0;
+	ssize_t n_read;
 	char thebu[BUFFER_SIZE];
 
 	if (cc != 3)
@@ -15,24 +16,22 @@ int main(int cc, char **cv)
 	f_frm = open(cv[1], O_RDONLY);
 	if (f_frm == -1)
 		dprintf(2, "Error: Can't read from file %s\n", cv[1]), exit(98);
-	f_to = open(cv[2], O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR
+	f_to = open(cv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR
 			| S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (f_to == -1)
 		dprintf(2, "Error: Can't write to %s\n", cv[2]), exit(99);
 	while ((n_read = read(f_frm, thebu, BUFFER_SIZE)) > 0)
 	{
-		if (write(f_to, thebu, n_read) == -1)
+		if (write(f_to, thebu, n_read) != n_read)
 			dprintf(2, "Error: Can't write to %s\n", cv[2]), exit(99);
 	}
-	if (close(f_frm) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", f_frm);
-		exit(100);
-	}
-	if (close(f_to) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", f_to);
-		exit(100);
-	}
-	return (0);
+	if (n_read == -1)
+		dprintf(2, "Error: Can't read from file %s\n", cv[1]), exit(98);
+	f_frm = close(f_frm);
+	f_to = close(f_to);
+	if (f_frm)
+		dprintf(2, "Error: Can't close fd %d\n", f_frm), exit(100);
+	if (f_to)
+		dprintf(2, "Error: Can't close fd %d\n", f_frm), exit(100);
+	return (EXIT_SUCCESS);
 }
