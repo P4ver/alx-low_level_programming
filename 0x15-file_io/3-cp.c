f0b47be8270662b6df85dd1ebcd133c3fe0cccb3
P4ver <s.pales20@gmail.com>
2023-10-03 11:53:38 -0700

    tst

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,9 @@
 #include "main.h"
 #define F_PERR (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+#define F_USG "Usage: %s file_from file_to\n"
+#define F_NR_ERR "Error: Can't read from file %s\n"
+#define F_NW_ERR "Error: Can't write to %s\n"
+#define F_NC_ERR "Error: Can't close fd %d\n"
 
 /**
  * main - the entry point,
@@ -14,25 +18,25 @@ int main(int cc, char **cv)
 	char thebu[BUFFER_SIZE];
 
 	if (cc != 3)
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", cv[0]), exit(97);
+		dprintf(STDERR_FILENO, F_USG, cv[0]), exit(97);
 	f_frm = open(cv[1], O_RDONLY);
 	if (f_frm == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", cv[1]), exit(98);
+		dprintf(STDERR_FILENO, F_NR_ERR, cv[1]), exit(98);
 	f_to = open(cv[2], O_WRONLY | O_CREAT | O_TRUNC, F_PERR);
 	if (f_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", cv[2]), exit(99);
+		dprintf(STDERR_FILENO, F_NW_ERR, cv[2]), exit(99);
 	while ((n_read = read(f_frm, thebu, BUFFER_SIZE)) > 0)
 	{
 		if (write(f_to, thebu, n_read) != n_read)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", cv[2]), exit(99);
+			dprintf(STDERR_FILENO, F_NW_ERR, cv[2]), exit(99);
 	}
 	if (n_read == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", cv[1]), exit(98);
+		dprintf(STDERR_FILENO, F_NR_ERR, cv[1]), exit(98);
 	f_frm = close(f_frm);
 	f_to = close(f_to);
 	if (f_frm)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_frm), exit(100);
+		dprintf(STDERR_FILENO, F_NC_ERR, f_frm), exit(100);
 	if (f_to)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_frm), exit(100);
+		dprintf(STDERR_FILENO, F_NC_ERR, f_frm), exit(100);
 	return (EXIT_SUCCESS);
 }
