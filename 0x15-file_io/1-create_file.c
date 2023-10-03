9a615573eb480a8845297fecf1f573b811b95d17
P4ver <s.pales20@gmail.com>
2023-10-03 06:51:23 -0700

    status

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <fcntl.h>
 /**
  * create_file - a function that creates a file,
  * @filename: the name of file (ptr)
