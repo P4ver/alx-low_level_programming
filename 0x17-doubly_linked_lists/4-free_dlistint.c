51033cfc879715f4a726518ea08d0271b19c2026
P4ver <s.pales20@gmail.com>
2023-11-09 16:39:28 -0800

    task 4 pvr

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -2,7 +2,10 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-/**/
+/**
+ * free_dlistint - function that frees a dlistint_t list,
+ * @head: pntr to head,
+ */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *tm_p;
