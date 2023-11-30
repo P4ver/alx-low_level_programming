860d3bc644a7ff7858a50e8b4ed66e9b98cbbf93
P4ver <s.pales20@gmail.com>
2023-11-30 11:40:11 -0800

    tsk pvr 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,11 +1,18 @@
 #include "hash_tables.h"
+/**
+ * hash_table_set - function that adds an element to the hash table,
+ * @ht: the hash tble to add or update,
+ * @key: key cannot be an empty str,
+ * @value: the value associated with key, and it can be an empty st,
+ * Return: 1 if it succeeded, or 0,
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int ndx_a;
 	hash_node_t *nwnd;
 
 	if (ht == NULL || key == NULL || *key == '\0')
-		return 0;
+		return (0);
 	ndx_a = key_index((unsigned char *)key, ht->size);
 	nwnd = malloc(sizeof(hash_node_t));
 
