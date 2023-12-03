c03df57359b108e0822935121e2599a965203958
P4ver <s.pales20@gmail.com>
2023-12-03 08:34:03 -0800

    tsk adv sec pvr

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int q;
+	hash_node_t *crn_a;
+	hash_node_t *next;
+
+	if (ht == NULL)
+		return;
+	for (q = 0; q < ht->size; ++q)
+	{
+		crn_a = ht->array[q];
+		while (crn_a != NULL)
+		{
+			next = crn_a->next;
+			free(crn_a->key);
+			free(crn_a->value);
+			free(crn_a);
+			crn_a = next;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
